e7d1cd1aecfa9b66e88c589c2d4c2dde94aaaba1
GlebTanaka <69252964+GlebTanaka@users.noreply.github.com>
2023-07-21 12:46:22 +0200

    Add html escaping example

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 #######################################################################################################################
 #                                                   ROUTING                                                           #
 #######################################################################################################################
 @app.route("/")
 def index():
     return 'Index Page'
+
 
 # @app.route("/login")
 # def login():
@@ -19,6 +21,17 @@ def index():
 def hello(name=None):
     return render_template('hello.html', name=name)
 
+
+#######################################################################################################################
+#                                                   HTML Escaping                                                     #
+#######################################################################################################################
+
+
+@app.route("/<name>")
+def escape_rout(name):
+    return f"Hello, {escape(name)}!"
+
+
 #######################################################################################################################
 #                                                   VARIABLES                                                         #
 #######################################################################################################################
@@ -26,25 +39,30 @@ def hello(name=None):
 @app.route('/user/<username>')
 def profile(username):
     # show the user profile for that user
-    return f'{username}\'s profile'
+    return f'{escape(username)}\'s profile'
+
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return f'Post {post_id}'
 
+
 @app.route('/path/<path:subpath>')
 def show_subpath(subpath):
     # show the subpath after /path/
     return f'Subpath {escape(subpath)}'
 
+
 #######################################################################################################################
 #                                                   Unique URLs                                                       #
 #######################################################################################################################
 
+
 @app.route('/projects/')
 def projects():
     return 'The project page'
+
 
 @app.route('/about')
 def about():
